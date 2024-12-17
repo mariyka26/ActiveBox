3d1024e7d776923fd9b3e42fb5db374f9a5497e2
mariyka26 <masha.zakharenko.masha@gmail.com>
2024-12-18 01:40:14 +0300

    testimonials

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,19 @@
 //     age: 23
 // }
 // console.log(user);
+
+var swiper = new Swiper(".swiper_container", {
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
+
+document.querySelector('.burger').addEventListener('click', function () {
+    this.classList.toggle('active');
+    document.querySelector('.header_navbar').classList.toggle('open');
+});
